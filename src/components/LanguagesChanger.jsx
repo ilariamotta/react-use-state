ad929a909d7c7962cd200743e7b11869550b5d89
ilariamotta <ilaria.motta93@gmail.com>
2025-11-28 16:03:45 +0100

    feat: funzione map

--- src/components/LanguagesChanger.jsx
+++ src/components/LanguagesChanger.jsx
@@ -1,10 +1,17 @@
-export default function LanguagesChanger () {
-return (
-<div className="container">
-    <button>Cliccami</button>
-    <div className="text-box">Ciao</div>
-</div>
+import { languages } from "../data/languages"
 
+export default function LanguagesChanger() {
+    return (
+        <div className="container">
+            <h1>Scegli il linguaggio!</h1>
+            <div className="flex">
+                {languages.map(({ title, id }) =>
+                (<button key={id}>{title}</button>
 
-)
+                ))}
+            </div>
+            <div className="text-box">Ciao</div>
+        </div>
+
+    )
 }
