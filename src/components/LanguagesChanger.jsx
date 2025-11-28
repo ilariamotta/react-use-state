c7b731d00cba73448bba63831f8106939590544f
ilariamotta <ilaria.motta93@gmail.com>
2025-11-28 16:44:44 +0100

    feat: cambio descrizione al click

--- src/components/LanguagesChanger.jsx
+++ src/components/LanguagesChanger.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react"
 import { languages } from "../data/languages"
 
 export default function LanguagesChanger() {
+    const [selectedIndex, setSelectedIndex] = useState(0);
     return (
         <div className="container">
             <h1>Scegli il linguaggio!</h1>
             <div className="flex">
-                {languages.map(({ title, id }) =>
-                (<button key={id}>{title}</button>
+                {languages.map(({ title}, index) =>
+                (<button key={index} onClick={() => setSelectedIndex(index)} className={selectedIndex === index ? "active" : ""}>{title}</button>
 
                 ))}
             </div>
-            <div className="text-box">Ciao</div>
+            <div className="text-box">{languages[selectedIndex].description}</div>
         </div>
 
     )
